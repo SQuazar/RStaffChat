a7c7fe10a1720e4a44574123fa48153ec035e73e
SQuazar <flawesv@yandex.ru>
2026-04-25 19:12:32 +0300

    Changed methods, added chat param

--- plugin/src/main/java/net/nullpointer/rstaffchat/plugin/service/StaffChatService.java
+++ plugin/src/main/java/net/nullpointer/rstaffchat/plugin/service/StaffChatService.java
@@ -1,13 +1,14 @@
 package net.nullpointer.rstaffchat.plugin.service;
 
+import net.nullpointer.rstaffchat.plugin.config.Chat;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.command.RemoteConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 public interface StaffChatService {
-    void broadcastStaff(Player player, String message);
+    void broadcastStaff(Player player, Chat chat, String message);
 
-    void broadcastStaff(ConsoleCommandSender sender, String message);
+    void broadcastStaff(ConsoleCommandSender sender, Chat chat, String message);
 
-    void broadcastStaff(RemoteConsoleCommandSender sender, String message);
+    void broadcastStaff(RemoteConsoleCommandSender sender, Chat chat, String message);
 }
