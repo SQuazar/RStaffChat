553af324022116ca908ec3b055749d4f0a0a2dbd
SQuazar <flawesv@yandex.ru>
2026-04-25 20:22:57 +0300

    Added update checker

--- plugin/src/main/java/net/nullpointer/rstaffchat/plugin/RStaffChatPlugin.java
+++ plugin/src/main/java/net/nullpointer/rstaffchat/plugin/RStaffChatPlugin.java
@@ -6,6 +6,7 @@ import net.nullpointer.rstaffchat.plugin.config.Chat;
 import net.nullpointer.rstaffchat.plugin.config.RStaffConfig;
 import net.nullpointer.rstaffchat.plugin.service.StaffChatService;
 import net.nullpointer.rstaffchat.plugin.service.StaffChatServiceImpl;
+import net.nullpointer.rstaffchat.plugin.util.UpdateChecker;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandMap;
@@ -14,10 +15,16 @@ import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
 public final class RStaffChatPlugin extends JavaPlugin {
+    private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern(
+            "dd.MM.yyyy HH:mm"
+    ).withZone(ZoneId.systemDefault());
+
     private RStaffConfig config;
     private RStaffChatAPI api;
     private StaffChatService staffChatService;
@@ -30,6 +37,17 @@ public final class RStaffChatPlugin extends JavaPlugin {
 
         setupService();
         registerCommands();
+
+        UpdateChecker.fetchUpdate(this, info -> {
+            String version = info.tag().substring(1);
+            if (!getPluginMeta().getVersion().equals(version)) {
+                getLogger().warning("New version for %s is available:".formatted(info.name()));
+                getLogger().warning(info.downloadUrl());
+                getLogger().warning("Published at: %s".formatted(FORMAT.format(info.publishedAt())));
+                if (info.preRelease())
+                    getLogger().warning("This is pre release version!");
+            }
+        });
     }
 
     private void setupService() {
