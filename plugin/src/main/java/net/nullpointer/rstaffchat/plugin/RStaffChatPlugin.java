0d8ae7e4156b8a9b3f878d9183b11bf62f4dab58
SQuazar <flawesv@yandex.ru>
2026-04-25 19:13:31 +0300

    Changed command for multiple chats

--- plugin/src/main/java/net/nullpointer/rstaffchat/plugin/RStaffChatPlugin.java
+++ plugin/src/main/java/net/nullpointer/rstaffchat/plugin/RStaffChatPlugin.java
@@ -2,15 +2,20 @@ package net.nullpointer.rstaffchat.plugin;
 
 import net.nullpointer.rstaffchat.api.RStaffChatAPI;
 import net.nullpointer.rstaffchat.core.RStaffChatAPIImpl;
+import net.nullpointer.rstaffchat.plugin.config.Chat;
 import net.nullpointer.rstaffchat.plugin.config.RStaffConfig;
 import net.nullpointer.rstaffchat.plugin.service.StaffChatService;
 import net.nullpointer.rstaffchat.plugin.service.StaffChatServiceImpl;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandMap;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Map;
 
 public final class RStaffChatPlugin extends JavaPlugin {
     private RStaffConfig config;
@@ -24,8 +29,7 @@ public final class RStaffChatPlugin extends JavaPlugin {
         config.load();
 
         setupService();
-
-        getCommand("staff").setExecutor(new RStaffCommand(staffChatService));
+        registerCommands();
     }
 
     private void setupService() {
@@ -54,6 +58,24 @@ public final class RStaffChatPlugin extends JavaPlugin {
         );
     }
 
+    private void registerCommands() {
+        for (Chat chat : config.getChats()) {
+            Bukkit.getCommandMap().register("rstaff", new StaffChatCommand(chat, staffChatService));
+        }
+    }
+
+    private void unregisterCommands() {
+        CommandMap map = Bukkit.getCommandMap();
+        List<String> remove = map.getKnownCommands().entrySet().stream()
+                .filter(e -> e.getValue() instanceof StaffChatCommand)
+                .map(Map.Entry::getKey)
+                .toList();
+        remove.forEach(key -> {
+            map.getCommand(key).unregister(map);
+            map.getKnownCommands().remove(key);
+        });
+    }
+
     @Override
     public void onDisable() {
         if (api != null) api.shutdown();
@@ -62,7 +84,9 @@ public final class RStaffChatPlugin extends JavaPlugin {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         reloadConfig();
+        unregisterCommands();
         config.load();
+        registerCommands();
         sender.sendMessage("§aКонфиг перезагружен!");
         return true;
     }
