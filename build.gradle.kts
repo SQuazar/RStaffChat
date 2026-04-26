e961cb5144438925b54f4f2407e9f0119f08ed8b
SQuazar <flawesv@yandex.ru>
2026-04-26 10:51:08 +0300

    Added publishing

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+group = "net.nullpointer.rstaffchat"
+version = "1.0.2"
+
+repositories {
+    mavenCentral()
+}
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+
+    group = rootProject.group
+    version = rootProject.version
+
+    configure<PublishingExtension> {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/SQuazar/RStaffChat")
+                credentials {
+                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                }
+            }
+        }
+    }
+}
